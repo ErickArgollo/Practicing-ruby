14d05e707b2603a89a2bee1d3e8c1257b00fdc2d
Erick Argollo <argolloerickdev@gmail.com>
2022-11-14 06:43:59 -0300

    feat: add tipos_e_variaveis and entrada_saida_padrao scripts.rb

---
+++ Udemy]/Entrada_saida_padrao.rb
@@ -0,0 +1,5 @@
+#Entrada puts ( quebra de linha ), print
+#Saida gets, method .chomp para retirar quebra de linha.
+print "Nome?"
+nome = gets.chomp
+print nome
---
+++ Udemy]/Ruby_Basico/Tipos_e_variaveis.rb
@@ -0,0 +1,24 @@
+=begin
+B
+=end 
+
+# Para saber o tipo, posso utilizar o .class, em JS seria (typeof). 
+
+firstString = 'Hi';
+firstInteger = 2;
+firstFloat = 2.3;
+# Em JS, o typeof seria Number, em ruby, Integer ou float.
+firstBool = true;
+print firstString.class, firstInteger.class
+puts firstBool # Aparentemente o .class não aponta como boolean, somente o valor, nesse caso, true.
+
+
+
+# Testes...
+
+firstArr = ['hi', 'teste', 2];
+
+print firstArr.class
+#Consigo saber se o tipo é array, utilizando o class ( no JS, voltaria como objeto, mas em ruby já tenho a resposta "correta".)
+
+# print firstArr[2] - a forma de acessar o valor da posição aparenta ser diferente, ( verificar mais a frente. )
