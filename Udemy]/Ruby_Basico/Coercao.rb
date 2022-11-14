3d167e6185bcb5a173b4761c92f03edd3155e1fd
Erick Argollo <argolloerickdev@gmail.com>
2022-11-14 06:57:29 -0300

    feat: add Interpolação and Coerção scripts

---
+++ Udemy]/Ruby_Basico/Coercao.rb
@@ -0,0 +1,15 @@
+# Conversão entre tipos. 
+=begin
+Mais utilizados
+.to_i
+.to_f
+.to_s
+=end
+
+myNum = 2.to_s
+# myNum.to_s Dessa forma aparentemente não funciona, devo alterar diretamente na minha variável para que a modificação aconteça.
+print myNum.class
+
+mySecondNum = 2.to_f
+
+#print mySecondNum.class
---
+++ Udemy]/Ruby_Basico/Interpolacao.rb
@@ -0,0 +1,5 @@
+# Para interpolar, é necessário a utilização de aspas duplas, de forma semelhante ao JS, só que no lugar do $, utilizarei #.
+
+firstName = "Erick"
+lastName = "Argollo"
+print "#{firstName} #{lastName}"
