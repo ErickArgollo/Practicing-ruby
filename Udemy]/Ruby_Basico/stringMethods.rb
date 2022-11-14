54e50b0602348425d0a20350627f6a45633b3dc6
Erick Argollo <argolloerickdev@gmail.com>
2022-11-14 16:04:59 -0300

    feat: add stringMethods script in Ruby_basico

---
+++ Udemy]/Ruby_Basico/stringMethods.rb
@@ -0,0 +1,10 @@
+"Hello".length # 5
+"Hello".reverse # “olleH”
+"Hello".upcase # “HELLO”
+"Hello".downcase # “hello”
+"hello".capitalize # “Hello”
+"Hello".include? "i"
+"Hello".gsub!(/e/, "o") # Hollo
+"1".to_i # transform string to integer –– 1
+"test".to_sym # converts to :test
+:test.to_s # converts to "test"
